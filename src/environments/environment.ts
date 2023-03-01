2fd9ba80f1009d35a4e004ae7d9a23d58294d02b
eyalg <narnavg@gmail.com>
2023-03-01 10:20:15 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+   apiKey: "AIzaSyB1HheHAPuXQJeCKeePw8om-Q9gYcF8Uq8",
+  authDomain: "upload-project-c7895.firebaseapp.com",
+  databaseURL: "https://upload-project-c7895-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "upload-project-c7895",
+  storageBucket: "upload-project-c7895.appspot.com",
+  messagingSenderId: "608176122060",
+  appId: "1:608176122060:web:d6d7cee61a64956cd9ba69",
+  measurementId: "G-BGVX7L6924"
+  }
+};
